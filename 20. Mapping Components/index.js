ab39fa4d0a998e71ced322105cd1b55505d2d0bd
Mehmet <mathdenizi@gmail.com>
2022-06-23 23:26:17 +0200

    Arrays.map in React

---
+++ 20. Mapping Components/index.js
@@ -0,0 +1,43 @@
+"use.strict";
+
+let jokesData = [
+  {
+    setup: "I got my daughter a fridge for her birthday.",
+    punchline: "I can't wait to see her face light up when she opens it.",
+  },
+  {
+    setup: "How did the hacker escape the police?",
+    punchline: "He just ransomware!",
+  },
+  {
+    setup: "Why don't pirates travel on mountain roads?",
+    punchline: "Scurvy.",
+  },
+  {
+    setup: "Why do bees stay in the hive in the winter?",
+    punchline: "Swarm.",
+  },
+  {
+    setup: "What's the best thing about Switzerland?",
+    punchline: "I don't know, but the flag is a big plus!",
+  },
+];
+
+function App() {
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
+}
+
+function Joke(p) {
+  return (
+    <div className="contact-card">
+      <h3 style={{ display: p.setup ? "block" : "none" }}>Setup:{p.setup}</h3>
+
+      <p>Punchline:{p.punchline}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
